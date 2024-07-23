1d066f91e0e59f0cd71fe81e9ccb1364b1d9e3b3
jhon-ace <justcodingph.97@gmail.com>
2024-07-23 14:03:15 +0800

    fromUB

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(\App\Http\Middleware\NoCacheMiddleware::class,);
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class,);
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class
         ]);
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,31 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "resources/js/bootstrap.js",
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/bootstrap.js',
             ],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // Allow Vite to be accessed from any IP
+        port: 5173, // Ensure the port matches your Vite server configuration
+        hmr: {
+            host: '192.168.33.11', // Adjust HMR host if necessary
+        },
+        cors: {
+            // origin: '*', // Allow all origins, adjust as needed
+            origin: 'http://192.168.33.11:8000', 
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+        },
+          
+    },
+    
+     base: '/',
 });
