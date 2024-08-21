e5f126f7842979d41bb8cbcf19d60e581dcd9843
jhonaces26 <iphitech.casabuena.jhonace@gmail.com>
2024-08-21 20:14:01 +0800

    21-8-24

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        // session()->forget('url.intended');
+        session()->forget('url.intended');
 
         if (Auth::check()) 
         {
